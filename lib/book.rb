215f2b1cac0882587849cfd55cd49cf0668329f5
BrendanFinnegan <bf289907@gmail.com>
2021-06-29 18:10:11 -0400

    commit tree 9a53226e9575c1b8031b80bb0e0c68a00f836e9c

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
 
+    end
+
+    def turn_page
+       puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+# , author_name, page_count, genre)
+#         @author_name = author_name
+#         @page_count = page_count
+#         @genre = genre
